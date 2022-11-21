730b49bc281bbe045a79add4249cfbe8cb6326fd
Axel918 <drakeaxel.mayo@benilde.edu.ph>
2022-11-21 15:08:48 +0800

    Fixed Santelmo Movement Pattern and Added Rotation Direction Change Feature

--- Lakbay sa Engkanto/Assets/Scripts/Enemies/Basic Enemies/Santelmo.cs
+++ Lakbay sa Engkanto/Assets/Scripts/Enemies/Basic Enemies/Santelmo.cs
@@ -10,17 +10,29 @@ public class Santelmo : Enemy
     [SerializeField] float angularSpeed = 5f;
 
     float posX, posY, angle = 0f;
+    Vector3 pos, velocity, scale;
+    [SerializeField] bool isClockwise;
 
     void Awake()
     {
         // Sets the center of rotation of the Santelmo to the parent transform 
         rotationCenter = transform.parent;
+
+        // Set Default Position and Scale Values
+        pos = transform.position;
+        scale = transform.localScale;
+
+        if (isClockwise)
+            angularSpeed *= -1;
+        else
+            angularSpeed *= 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         MovementPattern();
+        SpriteFlip();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -29,21 +41,44 @@ public class Santelmo : Enemy
             DamagePlayer(1f);
     }
 
-    #region EnemyMovement
+    #region Enemy Movement
     // Santelmo movement pattern,  moves in a circular pattern based from a center of rotation (rotationCenter)
     protected override void MovementPattern()
     {
         posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
         posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+
         transform.position = new Vector2(posX, posY);
         angle = angle + Time.deltaTime * angularSpeed;
 
-        if (posX > rotationRadius)
-            transform.rotation = Quaternion.Euler(0, 180f, 0);
-        else
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-
-        if (angle >= 360f) angle = 0f;
+        if (angle >= 360f) 
+            angle = 0f;
     }
     #endregion
+
+    /// <summary>
+    /// Santelmo Sprite Flipping System
+    /// </summary>
+    void SpriteFlip()
+    {
+        // Calculate Velocity
+        velocity = (transform.position - pos) / Time.deltaTime;
+        pos = transform.position;
+
+        // Flip Sprite Based on Horizontal Velocity
+        if (velocity.x > 0f)
+            transform.localScale = new Vector3(scale.x * -1f, scale.y, scale.z);
+        else
+            transform.localScale = new Vector3(scale.x * 1f, scale.y, scale.z);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (rotationCenter != null)
+        {
+            // Draws a blue line from this transform to the target
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, rotationCenter.position);
+        }
+    }
 }
