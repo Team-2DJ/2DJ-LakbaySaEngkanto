5737fa9b243754fe0a00c1b9c7b96a9222391f8c
Axel918 <drakeaxel.mayo@benilde.edu.ph>
2023-02-02 21:26:39 +0800

    Added Persistent Scene and Additive Scene Loading System

---
+++ Lakbay sa Engkanto/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Activate Main Menu
+        SingletonManager.Get<PanelManager>().ActivatePanel("Main Menu");
+    }
+
+    public void Play()
+    {
+        SingletonManager.Get<PanelManager>().ActivatePanel("Level Selection");
+    }
+
+    public void OnLevelSelected(int level)
+    {
+        SingletonManager.Get<PlayerData>().LevelIndex = level;
+
+        SingletonManager.Get<SceneLoader>().LoadScene("GameScene");
+    }
+}
