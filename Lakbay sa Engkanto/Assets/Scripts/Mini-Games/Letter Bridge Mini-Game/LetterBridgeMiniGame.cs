3d53f5fb615bf63970bf28903c9de55eecc205a0
Axel918 <drakeaxel.mayo@benilde.edu.ph>
2023-03-06 13:16:51 +0800

    Improved Naming Conventions for LetterBridgeMiniGame Script

--- Lakbay sa Engkanto/Assets/Scripts/Mini-Games/Letter Bridge Mini-Game/LetterBridgeMiniGame.cs
+++ Lakbay sa Engkanto/Assets/Scripts/Mini-Games/Letter Bridge Mini-Game/LetterBridgeMiniGame.cs
@@ -7,59 +7,62 @@ public class LetterBridgeMiniGame : MonoBehaviour
 {
     enum Direction
     {
-        LEFT,
-        RIGHT
+        LEFT = -1,
+        RIGHT = 1
     }
     
     [Header("References")]
     [SerializeField] private Transform startingPoint;
-    [SerializeField] private LetterItem letters;
+    [SerializeField] private LetterTile letters;
     [SerializeField] private TextMeshProUGUI wordToDisplay;
+    
+    [Header("Properties")]
     [SerializeField] private Direction direction;
-
-    [Header("Numbers")]
     [SerializeField] private string wordAnswer;
     [SerializeField] private int rows;
     [SerializeField] private float spacing;
     [SerializeField] [Range(0, 100)] private int probability;
-    [SerializeField] private int guaranteedIndex;
+    [SerializeField] private int defaultPityAmount;
 
-    private int currentIndex;
-    private List<LetterItem> letterList = new();
+    private int pityCounter;
+    private List<LetterTile> letterList = new();
     
     // Start is called before the first frame update
     void Start()
     {
-        currentIndex = Random.Range(0, guaranteedIndex + 1);
-
-        if (direction == Direction.LEFT)
-            spacing *= -1;
-        else if (direction == Direction.RIGHT)
-            spacing *= 1;
+        pityCounter = Random.Range(0, defaultPityAmount + 1);
 
         // Display Word Answer to the "Word To Display" Text
         wordToDisplay.text = wordAnswer;
-        
+
+        SpawnLetterTiles();
+        SetProbability();
+    }
+
+    void SpawnLetterTiles()
+    {
         for (int i = 0; i < rows; i++)
         {
-            LetterItem tile = Instantiate(letters.gameObject, startingPoint.position, Quaternion.identity).GetComponent<LetterItem>();
+            // Set Position
+            Vector2 position = new Vector2(startingPoint.position.x + (i * (spacing * (float)direction)), startingPoint.position.y);
+
+            // Spawn the Letter Tile and Set Parent to this GameObject
+            LetterTile letterTile = Instantiate(letters.gameObject, position, Quaternion.identity).GetComponent<LetterTile>();
+            letterTile.transform.SetParent(transform);
 
             // Add Letter Tile to the List
-            letterList.Add(tile);
-
-            // X Position
-            float x = startingPoint.position.x + (i * spacing);
-
-            // Set Position of LetterItem
-            tile.transform.position = new Vector2(x, startingPoint.position.y);
+            letterList.Add(letterTile);
         }
+    }
 
-        foreach (LetterItem item in letterList)
+    void SetProbability()
+    {
+        foreach (LetterTile item in letterList)
         {
-            if (currentIndex-- <= 0)
+            if (pityCounter-- <= 0)
             {
                 item.SetChance(100, wordAnswer);
-                currentIndex = Random.Range(0, guaranteedIndex + 1);
+                pityCounter = Random.Range(0, defaultPityAmount + 1);
             }
             else
             {
--- Lakbay sa Engkanto/Assets/Scripts/Mini-Games/Letter Bridge Mini-Game/LetterTile.cs
+++ Lakbay sa Engkanto/Assets/Scripts/Mini-Games/Letter Bridge Mini-Game/LetterTile.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class LetterItem : MonoBehaviour
+public class LetterTile : MonoBehaviour
 {
     public bool IsCorrect { get; private set; }
     public TextMeshProUGUI LetterText;
