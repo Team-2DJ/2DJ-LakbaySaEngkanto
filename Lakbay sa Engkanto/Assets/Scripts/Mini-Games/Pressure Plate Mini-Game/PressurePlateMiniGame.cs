00e7f19fa1a92cf425129182b981a6a962dd4c4d
DavidGutierrez-Benilde <davidandrew.gutierrez@benilde.edu.ph>
2023-03-16 00:18:06 +0800

    Added PressurePlate Randomizing

--- Lakbay sa Engkanto/Assets/Scripts/Mini-Games/Pressure Plate Mini-Game/PressurePlateMiniGame.cs
+++ Lakbay sa Engkanto/Assets/Scripts/Mini-Games/Pressure Plate Mini-Game/PressurePlateMiniGame.cs
@@ -1,23 +1,55 @@
 using UnityEngine;
+using System.Collections.Generic;
 using TMPro;
 
 public class PressurePlateMiniGame : MonoBehaviour
 {
     [Header("Object Setup")]
-
-    [SerializeField] private TextMeshProUGUI questionTextBox;
     [SerializeField] private string id;
+    [SerializeField] private TextMeshProUGUI questionTextBox;
     [SerializeField] private PressurePlate[] pressurePlates;
 
+    [Space]
     [Header("Gameplay Settings")]
+    [SerializeField] private string doorToOpen;
     [SerializeField] string questionText;
+    [SerializeField] string correctAnswer;
+    [SerializeField] private List<string> wrongAnswers = new();
 
     private void Start()
     {
         questionTextBox.text = questionText;
+
+        foreach (var pressurePlate in pressurePlates)
+            pressurePlate.SetID(id);
+
+        RandomizePressurePlateAnswers();
     }
 
+    /// <summary>
+    /// Randomizes the answer each pressure plate has
+    /// </summary>
+    private void RandomizePressurePlateAnswers()
+    {
+        // Gets a random pressurePlate from the pressurePlate array;
+        int correctIndex = Random.Range(0, pressurePlates.Length);
 
-    // Might implement a randomzing pressure plate system idk? 
+        // Gets the pressurePlate from the correctIndex int and
+        // then initializes it to be correct
+        pressurePlates[correctIndex].Initialize(doorToOpen, Type.CORRECT, correctAnswer);
+
+        // This code block is used by the pressure plates which are wrong
+        foreach (PressurePlate pressurePlate in pressurePlates)
+        {
+            // If the pressurePlate is correct iterate (skip) over it
+            if (pressurePlate.type == Type.CORRECT) continue;
+
+            int index = Random.Range(0, wrongAnswers.Count);
+
+            // Initializes the wrong answers found within the array 
+            pressurePlate.Initialize(doorToOpen, Type.INCORRECT, wrongAnswers[index]);
+            wrongAnswers.RemoveAt(index);
+        }
+    }
 
 }
