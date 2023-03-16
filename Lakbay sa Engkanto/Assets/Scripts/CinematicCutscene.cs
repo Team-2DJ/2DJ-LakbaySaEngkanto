6a9ccae6c1593d5e6ef7b7e7528dac08c64ba12f
Axel918 <drakeaxel.mayo@benilde.edu.ph>
2023-03-16 22:34:21 +0800

    Renamed Interactive Cutscene to Cinematic Cutscene

--- Lakbay sa Engkanto/Assets/Scripts/CinematicCutscene.cs
+++ Lakbay sa Engkanto/Assets/Scripts/CinematicCutscene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class InteractiveCutscene : Cutscenes
+public class CinematicCutscene : Cutscenes
 {
     [SerializeField] private Sprite[] panelSprites;
 
