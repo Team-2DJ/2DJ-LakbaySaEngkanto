cdcdc82101689e1dcb2f02f26a3ec2f8d5180d4c
DavidGutierrez-Benilde <davidandrew.gutierrez@benilde.edu.ph>
2023-03-17 23:57:12 +0800

    Added Barebones Podium Slot, fixed BookSlot code

--- Lakbay sa Engkanto/Assets/Scripts/Mini-Games/Podium Mini-Game/PodiumSlot.cs
+++ Lakbay sa Engkanto/Assets/Scripts/Mini-Games/Podium Mini-Game/PodiumSlot.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class BookSlot : MonoBehaviour, IDropHandler, IPointerExitHandler
+[RequireComponent(typeof(RectTransform))]
+public class PodiumSlot : MonoBehaviour, IDropHandler, IPointerExitHandler
 {
     [Header("Object Setup")]
     [SerializeField] string id;
@@ -9,31 +10,22 @@ public class BookSlot : MonoBehaviour, IDropHandler, IPointerExitHandler
     [Header("Gameplay Settings")]
     [SerializeField] string bookTitle;
 
-    private bool isOccupied;
-    public bool IsRight { get; private set; }
     private RectTransform rectTransform;
-
-    private void Start()
-    {
-        rectTransform = GetComponent<RectTransform>();
-    }
-
-    public void Initialize(string _id, string _bookTitle)
-    {
-        id = _id;
-        bookTitle = _bookTitle;
-    }
+    public bool IsRight { get; private set; }
 
     private void OnEnable()
     {
         SingletonManager.Get<PlayerEvents>().OnPlayerPlacedItem += CheckAnswer;
-        SingletonManager.Get<GameEvents>().OnSetCondition += ResetBookSlot;
     }
 
     private void OnDisable()
     {
         SingletonManager.Get<PlayerEvents>().OnPlayerPlacedItem -= CheckAnswer;
-        SingletonManager.Get<GameEvents>().OnSetCondition -= ResetBookSlot;
+    }
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
     }
 
     /// <summary>
@@ -45,7 +37,6 @@ public class BookSlot : MonoBehaviour, IDropHandler, IPointerExitHandler
         // if an object is dropped, then pointerDrag != null
         if (eventData.pointerDrag != null)
         {
-            isOccupied = true;
 
             RectTransform droppedObject = eventData.pointerDrag.GetComponent<RectTransform>();
 
@@ -53,7 +44,6 @@ public class BookSlot : MonoBehaviour, IDropHandler, IPointerExitHandler
             droppedObject.anchoredPosition = rectTransform.anchoredPosition;
         }
     }
-
 
     /// <summary>
     /// Inherited from IPointerExitHandler, 
@@ -63,8 +53,6 @@ public class BookSlot : MonoBehaviour, IDropHandler, IPointerExitHandler
     {
         if (eventData.pointerDrag != null)
         {
-            isOccupied = false;
-            IsRight = false;
         }
     }
 
@@ -74,25 +62,9 @@ public class BookSlot : MonoBehaviour, IDropHandler, IPointerExitHandler
     /// <param name="bookTitle">The Title of the Book</param>
     private void CheckAnswer(string bookTitle)
     {
-        // if the bookSlot is currently not occupied, then return; 
-        if (!isOccupied) return;
-
         // if the title of the book doesn't correspond with this gameObject, then return; 
         if (bookTitle != this.bookTitle) return;
 
         IsRight = true;
-    }
-
-    /// <summary>
-    /// Resets the BookSlot back to its initial values
-    /// </summary>
-    /// <param name="dontReset">conditional</param>
-    private void ResetBookSlot(string id, bool dontReset)
-    {
-        if (id != this.id) return;
-        if (dontReset) return;
-
-        isOccupied = false;
-        IsRight = false;
     }
 }
